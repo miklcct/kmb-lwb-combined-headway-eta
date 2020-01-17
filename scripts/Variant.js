94fd91f171aada01e6fd313b8ded0542792c631d
Michael Tsang <miklcct@gmail.com>
2020-01-17 12:08:40 +0800

    add more JSDoc

--- scripts/Variant.js
+++ scripts/Variant.js
@@ -1,7 +1,16 @@
 'use strict';
 
 class Variant {
-    constructor(/** Route */ route, /** string */ id, /** int */ sequence, /** string */ description, /** ?string */ info) {
+    /**
+     * Create a route variant
+     *
+     * @param {!Route} route The route which the variant belongs to
+     * @param {string} id The variant ID (RDV), e.g. 788-MAF-1
+     * @param {int} sequence The number of the variant
+     * @param {string} description The description of the variant, e.g. "Normal routeing"
+     * @param {?string} info The internal code to be used to get stop list
+     */
+    constructor(route, id, sequence, description, info) {
         this.route = route;
         this.id = id;
         this.sequence = sequence;
@@ -9,15 +18,21 @@ class Variant {
         this.info = info;
     }
 }
-Variant.all = {};
-Variant.get = function (/** Route */ route, /** Function */ callback) {
+
+/**
+ * Get the list of variants from a route
+ *
+ * @param {Route} route
+ * @param {function(!Object<string, !Variant>)} callback
+ */
+Variant.get = function (route, callback) {
     Common.callApi(
         'getvariantlist.php'
         , {id : route.id}
-        , function (/** Array */ data) {
+        , function (data) {
             const variants = {};
             data.forEach(
-                function (/** Array */ segments) {
+                function (segments) {
                     const variant = new Variant(route, segments[2], Number(segments[0]), segments[3], segments[4]);
                     variants[variant.id] = variant;
                 }
