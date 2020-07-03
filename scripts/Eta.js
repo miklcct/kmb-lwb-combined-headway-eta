ef67c51ab1c643e775a8254189a3345f05298fbc
Michael Tsang <miklcct@gmail.com>
2020-07-03 20:31:54 +0800

    allow choosing API

--- scripts/Eta.js
+++ scripts/Eta.js
@@ -19,6 +19,11 @@ class Eta {
     }
 }
 
+Eta.WEB_API = 1;
+Eta.MOBILE_API = 2;
+
+Eta.API_USED = Eta.WEB_API;
+
 /**
  * Compare two ETA entries by time
  *
@@ -37,60 +42,83 @@ Eta.compare = function (a, b) {
  * @param {function(Array<Eta>)} callback
  */
 Eta.get = function (stopRoute, callback) {
-    /** @type {!Date} */
-    const current_date = new Date;
-    /** @type {string} */
-    const date_string = current_date.getUTCFullYear() + "-" + ("00" + (current_date.getUTCMonth() + 1)).slice(-2) + "-" + ("00" + current_date.getUTCDate()).slice(-2) + " " + ("00" + current_date.getUTCHours()).slice(-2) + ":" + ("00" + current_date.getUTCMinutes()).slice(-2) + ":" + ("00" + current_date.getUTCSeconds()).slice(-2) + "." + ("00" + current_date.getUTCMilliseconds()).slice(-2) + ".";
-    /** @type {string} */
-    const sep = "--31" + date_string + "13--";
-    var token = "EA" + btoa(stopRoute.variant.route.number + sep + stopRoute.variant.route.bound + sep + stopRoute.variant.serviceType + sep + stopRoute.stop.id.trim().replace(/-/gi, '') + sep + stopRoute.sequence + sep + (new Date).getTime());
-    $.post(
-        (location.protocol === 'https:' ? Common.PROXY_URL : '') + Common.API_ENDPOINT + "?action=get_ETA&lang=0"
-        , {
-            token : token,
-            t : date_string,
-        }
-        /**
-         *
-         * @param {object[]} json.data.response
-         */
-        , function (json) {
-            callback(
-                json.data.response
-                    .map(
-                        /**
-                         *
-                         * @param {String} obj.t
-                         * @param {String} obj.eot
-                         * @param {int} obj.dis
-                         */
-                        obj => (
-                            {
-                                time : obj.t.substr(0, 5),
-                                remark : obj.t.substr(5),
-                                real_time : typeof obj.dis === 'number',
-                                distance : obj.dis === undefined ? null : obj.dis,
-                            }
-                        )
-                    )
-                    .filter(obj => obj.time.match(/^[0-9][0-9]:[0-9][0-9]$/) !== null)
-                    .map(
-                        obj => {
-                            const time = new Date();
-                            time.setHours(Number(obj.time.split(':')[0]), Number(obj.time.split(':')[1]));
-                            if (time.getTime() - Date.now() < -60 * 60 * 1000 * 2) {
-                                // the time is less than 2 hours past - assume midnight rollover
-                                time.setDate(time.getDate() + 1);
-                            }
-                            if (time.getTime() - Date.now() > 60 * 60 * 1000 * 6) {
-                                // the time is more than 6 hours in the future - assume midnight rollover
-                                time.setDate(time.getDate() - 1);
-                            }
-                            return new Eta(stopRoute, time, obj.distance, obj.remark, obj.real_time);
+    /**
+     *
+     * @param {object[]} json.response
+     * @param {object[]} json.data.response
+     */
+    const handler = function (json) {
+        callback(
+            (Eta.API_USED === Eta.WEB_API
+                ? json.data.response
+                : Eta.API_USED === Eta.MOBILE_API ? json.response : []
+            )
+                .map(
+                    /**
+                     *
+                     * @param {String} obj.t
+                     * @param {String} obj.eot
+                     * @param {int} obj.dis
+                     */
+                    obj => (
+                        {
+                            time : obj.t.substr(0, 5),
+                            remark : obj.t.substr(5),
+                            real_time : typeof obj.dis === 'number',
+                            distance : obj.dis === undefined ? null : obj.dis,
                         }
                     )
-            );
-        }
-    );
+                )
+                .filter(obj => obj.time.match(/^[0-9][0-9]:[0-9][0-9]$/) !== null)
+                .map(
+                    obj => {
+                        const time = new Date();
+                        time.setHours(Number(obj.time.split(':')[0]), Number(obj.time.split(':')[1]));
+                        if (time.getTime() - Date.now() < -60 * 60 * 1000 * 2) {
+                            // the time is less than 2 hours past - assume midnight rollover
+                            time.setDate(time.getDate() + 1);
+                        }
+                        if (time.getTime() - Date.now() > 60 * 60 * 1000 * 6) {
+                            // the time is more than 6 hours in the future - assume midnight rollover
+                            time.setDate(time.getDate() - 1);
+                        }
+                        return new Eta(stopRoute, time, obj.distance, obj.remark, obj.real_time);
+                    }
+                )
+        );
+    };
+    if (Eta.API_USED === Eta.WEB_API) {
+        /** @type {!Date} */
+        const current_date = new Date;
+        /** @type {string} */
+        const date_string = current_date.getUTCFullYear() + "-" + ("00" + (current_date.getUTCMonth() + 1)).slice(-2) + "-" + ("00" + current_date.getUTCDate()).slice(-2) + " " + ("00" + current_date.getUTCHours()).slice(-2) + ":" + ("00" + current_date.getUTCMinutes()).slice(-2) + ":" + ("00" + current_date.getUTCSeconds()).slice(-2) + "." + ("00" + current_date.getUTCMilliseconds()).slice(-2) + ".";
+        /** @type {string} */
+        const sep = "--31" + date_string + "13--";
+        var token = "EA" + btoa(stopRoute.variant.route.number + sep + stopRoute.variant.route.bound + sep + stopRoute.variant.serviceType + sep + stopRoute.stop.id.trim().replace(/-/gi, '') + sep + stopRoute.sequence + sep + (new Date).getTime());
+        $.post(
+            (location.protocol === 'https:' ? Common.PROXY_URL : '') + Common.API_ENDPOINT + "?action=get_ETA&lang=0"
+            , {
+                token : token,
+                t : date_string,
+            }
+            , handler
+        );
+    } else if (Eta.API_USED === Eta.MOBILE_API) {
+        $.get(
+            Common.PROXY_URL + 'https://etav3.kmb.hk/'
+            , {
+                action : 'geteta',
+                route : stopRoute.variant.route.number,
+                bound : stopRoute.variant.route.bound,
+                stop_seq : stopRoute.sequence,
+                serviceType : stopRoute.variant.serviceType,
+                lang : 'en',
+                updated : '',
+            }
+            , handler
+        );
+    } else {
+
+    }
 };
 
