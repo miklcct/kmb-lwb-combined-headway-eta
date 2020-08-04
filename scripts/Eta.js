0c56400196155a9e92786f0c74b082671ecc4a65
Michael Tsang <miklcct@gmail.com>
2020-08-04 13:36:40 +0800

    retry with another api key on 403

--- scripts/Eta.js
+++ scripts/Eta.js
@@ -45,47 +45,52 @@ Eta.compare = function (a, b) {
 Eta.get = function (stopRoute, callback) {
     /**
      * @param {array|object} json
+     * @param {XMLHttpRequest} jqXHR
      * @param {object[]} json.data.response
      */
-    const handler = function (json) {
-        callback(
-            (Eta.API_USED === Eta.WEB_API
-                ? json.data.response
-                : Eta.API_USED === Eta.MOBILE_API ? json[0].eta : []
-            )
-                .map(
-                    /**
-                     *
-                     * @param {String} obj.t
-                     * @param {String} obj.eot
-                     * @param {int} obj.dis
-                     */
-                    obj => (
-                        {
-                            time : obj.t.substr(0, 5),
-                            remark : obj.t.substr(5),
-                            real_time : typeof obj.dis === 'number',
-                            distance : obj.dis === undefined ? null : obj.dis,
+    const handler = function (json, jqXHR) {
+        if (jqXHR.readyState === 4 && jqXHR.status === 403) {
+            Eta.get(stopRoute, callback);
+        } else {
+            callback(
+                (Eta.API_USED === Eta.WEB_API
+                        ? json.data.response
+                        : Eta.API_USED === Eta.MOBILE_API ? json[0].eta : []
+                )
+                    .map(
+                        /**
+                         *
+                         * @param {String} obj.t
+                         * @param {String} obj.eot
+                         * @param {int} obj.dis
+                         */
+                        obj => (
+                            {
+                                time : obj.t.substr(0, 5),
+                                remark : obj.t.substr(5),
+                                real_time : typeof obj.dis === 'number',
+                                distance : obj.dis === undefined ? null : obj.dis,
+                            }
+                        )
+                    )
+                    .filter(obj => obj.time.match(/^[0-9][0-9]:[0-9][0-9]$/) !== null)
+                    .map(
+                        obj => {
+                            const time = new Date();
+                            time.setHours(Number(obj.time.split(':')[0]), Number(obj.time.split(':')[1]));
+                            if (time.getTime() - Date.now() < -60 * 60 * 1000 * 2) {
+                                // the time is less than 2 hours past - assume midnight rollover
+                                time.setDate(time.getDate() + 1);
+                            }
+                            if (time.getTime() - Date.now() > 60 * 60 * 1000 * 6) {
+                                // the time is more than 6 hours in the future - assume midnight rollover
+                                time.setDate(time.getDate() - 1);
+                            }
+                            return new Eta(stopRoute, time, obj.distance, obj.remark, obj.real_time);
                         }
                     )
-                )
-                .filter(obj => obj.time.match(/^[0-9][0-9]:[0-9][0-9]$/) !== null)
-                .map(
-                    obj => {
-                        const time = new Date();
-                        time.setHours(Number(obj.time.split(':')[0]), Number(obj.time.split(':')[1]));
-                        if (time.getTime() - Date.now() < -60 * 60 * 1000 * 2) {
-                            // the time is less than 2 hours past - assume midnight rollover
-                            time.setDate(time.getDate() + 1);
-                        }
-                        if (time.getTime() - Date.now() > 60 * 60 * 1000 * 6) {
-                            // the time is more than 6 hours in the future - assume midnight rollover
-                            time.setDate(time.getDate() - 1);
-                        }
-                        return new Eta(stopRoute, time, obj.distance, obj.remark, obj.real_time);
-                    }
-                )
-        );
+            );
+        }
     };
     if (Eta.API_USED === Eta.WEB_API) {
         /** @type {!Date} */
@@ -117,6 +122,11 @@ Eta.get = function (stopRoute, callback) {
             ctr : secret.ctr
         };
         const encrypted_query = Secret.getSecret('?' + new URLSearchParams(query).toString(), secret.ctr);
+        const retry = /** XMLHttpRequest */ jqXHR => {
+            if (jqXHR.readyState === 4 && jqXHR.status === 403) {
+                Eta.get(stopRoute, callback);
+            }
+        }
         if (Eta.MOBILE_API_METHOD === 'POST') {
             $.post(
                 {
@@ -128,11 +138,12 @@ Eta.get = function (stopRoute, callback) {
                         }
                     ),
                     success : handler,
+                    error : retry,
                     contentType : 'application/json',
                 }
             );
         } else {
-            $.get(Common.PROXY_URL + 'https://etav3.kmb.hk/?action=geteta', query, handler)
+            $.get(Common.PROXY_URL + 'https://etav3.kmb.hk/?action=geteta', query, handler).fail(retry);
         }
     } else {
 
--- scripts/ui.js
+++ scripts/ui.js
@@ -35,11 +35,13 @@ Date.prototype.hhmmss = function () {
     $(document).ajaxError(
         function (/** Event */ event, /** XMLHttpRequest */ jqXHR, /** Object */ ajaxSettings) {
             if (jqXHR.readyState === 4 && jqXHR.status < 500) {
-                const $failure = $('#failure');
-                $failure.append($('<span/>').text(('AJAX call to ' + ajaxSettings.url + ' failed: ' + jqXHR.status).trim()))
-                    .append($('<br/>'));
-                $failure.css('display', 'block');
-                debugger;
+                if (jqXHR.status !== 403) {
+                    const $failure = $('#failure');
+                    $failure.append($('<span/>').text(('AJAX call to ' + ajaxSettings.url + ' failed: ' + jqXHR.status).trim()))
+                        .append($('<br/>'));
+                    $failure.css('display', 'block');
+                    debugger;
+                }
             } else {
                 setTimeout(
                     function () {
